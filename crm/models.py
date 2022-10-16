3e42eaf132eee6696403924a541fd910a5689694
Sanal Bogdo <sanal@bogdo.name>
2022-10-16 15:24:05 +0300

    first commit

--- crm/models.py
+++ crm/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class StatusCrm(models.Model):
+    status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+        
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True) 
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
 
     def __str__(self):
         return self.order_name
